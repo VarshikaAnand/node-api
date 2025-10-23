c1191df46df7e98fa7864e13fd6c2409960bec49
VarshikaAnand <varshikaanand05@gmail.com>
2025-10-23 09:24:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'APP is running' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
